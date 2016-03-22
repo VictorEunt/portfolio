3454a522c8aba7953e211a36c300d1874b96801f
VictorEunt <VictorEstrada@my.unt.edu>
2016-03-22 00:50:10 -0500

    Updated contact and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :friends
+
   root "pages#home"
   get "about" => "pages#about"
-		get "contact" => "pages#contact"
+	get "contact" => "pages#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
