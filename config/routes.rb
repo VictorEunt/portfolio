2b3f7618f8ea3c4a82b2db2cb545ceb0fc24eddc
VictorEunt <VictorEstrada@my.unt.edu>
2016-03-23 17:05:09 -0500

    adjusted contact layout - rendering form to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "about" => "pages#about"
-	get "contact" => "pages#contact"
+  get "contact" => "friends#new"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
