9c0daf5b68db15602cdf3697deb791c4ef3bbae1
VictorEunt <VictorEstrada@my.unt.edu>
2016-03-23 19:22:41 -0500

    added table clients

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317201352) do
+ActiveRecord::Schema.define(version: 20160324002055) do
+
+  create_table "clients", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "subject"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friends", force: true do |t|
     t.string   "name"
