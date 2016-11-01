6020d87669b290e6671f40a20b5fa3213505288b
VictorEunt <VictorEstrada@my.unt.edu>
2016-11-01 00:40:26 -0500

    created techcorp project page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "about" => "pages#about"
   get "projects" => "pages#projects"
   get "contact" => "clients#new"
+  get "techcorp" => "pages#techcorp"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
