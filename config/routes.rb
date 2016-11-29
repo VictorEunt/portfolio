c775f52eaac39e63e882fa3c742ade836c6e9c14
VictorEunt <VictorEstrada@my.unt.edu>
2016-11-29 02:23:58 -0600

    upadated overture project and footer

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "contact" => "clients#new"
   get "techcorp" => "pages#techcorp"
   get "americanheart" => "pages#americanheart"
+  get "overturemarketplace" => "pages#overturemarketplace"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
